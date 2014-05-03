2541fdd3dad7151c5b22c621c571fed11c5a4611
Alenkka <metoda1@ya.ru>
2014-05-03 21:47:49 +0400

    comment

--- src/Server.java
+++ src/Server.java
@@ -5,6 +5,7 @@ import java.net.InetSocketAddress;
 
 public class Server {
 
+    //comment
     public static void main(String[] args) throws IOException {
         HttpServer server = HttpServer.create(new InetSocketAddress(8000), 0);
         HttpContext context = server.createContext("/letsPlay/", new MyHandler());
